1500ccd71a780e553970b52b305eacf380136809
Matthew <Matthew.Abentajado@gmail.com>
2026-03-07 11:05:03 -0600

    primer commit del proyecto

---
+++ src/TalonarioConsola/TalonarioConsola.java
@@ -0,0 +1,11 @@
+package TalonarioConsola;
+
+public class TalonarioConsola {
+	
+	public static void main(String[] args) {
+		//TODO Auto_generated method stub
+		
+		System.out.println("hola");
+	}
+
+}
